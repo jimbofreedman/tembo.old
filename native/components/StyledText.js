20e207a087fbdc9acfb0d3c5dd8b92341ab1606b
Jimbo Freedman <jimbo@jimbo.rocks>
2020-05-13 21:09:43 -0600

    Remove Native

--- native/components/StyledText.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Text } from 'react-native';
-
-// eslint-disable-next-line import/prefer-default-export
-export function MonoText(props) {
-  const { style } = props;
-  return <Text {...props} style={[style, { fontFamily: 'space-mono' }]} />;
-}
